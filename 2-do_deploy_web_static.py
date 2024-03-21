4c666a2d658835b11999b0f97ee3dca58f9cfaf7
Markcino <33788083+Markcino@users.noreply.github.com>
2024-03-21 11:51:38 +0000

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,9 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from fabric.api import put, run, env, sudo
 from os.path import exists
 env.hosts = ['100.25.109.78', '34.229.69.154']
-
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
@@ -18,13 +17,13 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        sudo('mkdir -p {}{}/'.format(path, no_ext))
+        sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        sudo('rm /tmp/{}'.format(file_n))
+        sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        sudo('rm -rf {}{}/web_static'.format(path, no_ext))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
