6be190f0145287042702e000781ed2763e0cc944
Markcino <33788083+Markcino@users.noreply.github.com>
2024-03-22 13:15:17 +0000

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['100.25.109.78', '34.229.69.154']
+env.hosts = ['54.196.45.126', '100.25.110.105']
 
 
 def do_pack():
@@ -34,13 +34,13 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        sudo('mkdir -p {}{}/'.format(path, no_ext))
-        sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        sudo('rm /tmp/{}'.format(file_n))
-        sudo('rsync -a {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        sudo('rm -rf {}{}/web_static'.format(path, no_ext))
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
